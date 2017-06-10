114c54817acf4f91f6578624fe25151cedfff89a
Hannah Eyre <hannahreyre@gmail.com>
2017-06-09 18:13:13 -0600

    started the stuff for the summer camp

---
+++ src/summerCamp/Day1.java
@@ -0,0 +1,10 @@
+package summerCamp;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/summerCamp/Day2.java
@@ -0,0 +1,46 @@
+package summerCamp;
+
+public class Day2 {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] t = {"hello", "what"};
+		System.out.println(t.toString());
+	}
+	
+	/**
+	 * Print the contents of an array to the console.
+	 * 
+	 * @param array An array of strings to print.
+	 */
+	public static void printArray(String[] array) {
+		
+		
+	}
+	
+	/**
+	 * Fill every element in an array with a word.
+	 * 
+	 * @param array An array to fill with 
+	 * @param word
+	 */
+	public static void fillArray(String[] array, String word) {
+		
+		printArray(array);
+	}
+	
+	public static void fillArrayBeginning(String[] array, String word, int num) {
+		
+	}
+
+	public static void findWord(String[] array, String word) {
+		
+		
+	}
+	
+	public static void reverseArray(String[] array) {
+		
+		printArray(array);
+	}
+
+}
